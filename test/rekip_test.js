0fac322117f222f8543dbf4a018c9c9baa93361f
Leny <info@flatland.be>
2014-06-07 17:45:08 +0200

    Add tests

--- test/rekip_test.js
+++ test/rekip_test.js
@@ -5,12 +5,21 @@ var rekip = require( "../lib/rekip.js" ),
 
 exports[ "rekip" ] = {
   "basic": function( test ) {
-    test.expect( 1 );
-    rekip( "http://placehold.it/150x150", "test/fixtures/", function() {
+    test.expect( 2 );
+    rekip( "http://placehold.it/150x150", "test/fixtures/150x150.gif", function( oError ) {
       var bfExpectedFile = fs.readFileSync( "test/expected/150x150.gif" ),
           bfResultFile = fs.readFileSync( "test/fixtures/150x150.gif" );
+      test.equal( oError, null, "should be null." );
       test.deepEqual( bfResultFile, bfExpectedFile, "downloaded file should be the same as expected.");
       test.done();
     } );
   },
+  "error": function( test ) {
+    test.expect( 2 );
+    rekip( "http://thissitedoesntexists.nul/unknown.test", "test/fixtures/unknown.gif", function( oError ) {
+      test.notEqual( oError, null, "shouldn't be null." );
+      test.equal( oError.code, "ENOTFOUND", "should be 'ENOTFOUND'." );
+      test.done();
+    } );
+  }
 };
